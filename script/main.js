fc9d102426d155226b955aafc1fb4795324fb9b4
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2023-01-02 22:36:51 +0300

    mobile menu

--- script/main.js
+++ script/main.js
@@ -1,5 +1,7 @@
+//mobile menu
 const button = document.querySelector(".burger");
 
-button.addEventListener("click", () => {
+button?.addEventListener("click", () => {
   button.classList.toggle("active");
+  document.querySelector('.menu').classList.toggle('opened');
 });
