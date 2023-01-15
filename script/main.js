82ede466569092c38849365a01861eb5641c7928
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2023-01-15 19:18:59 +0300

    accordeon

--- script/main.js
+++ script/main.js
@@ -5,3 +5,16 @@ button?.addEventListener("click", () => {
   button.classList.toggle("active");
   document.querySelector('.menu').classList.toggle('opened');
 });
+
+//ACCORDEON
+var reviewsHomeMoreBtn = document.querySelector(".reviews__more-btn");
+
+reviewsHomeMoreBtn.addEventListener('click', () => {
+  reviewsHomeMoreBtn.classList.toggle('active');
+  const panel = document.querySelector('.reviews__list .more-list')
+  if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+  } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+  };
+})
